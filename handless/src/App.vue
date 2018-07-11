5ac283f5ee93ea6ed10b169bdc250cc40572c858
waiss67 <baqui.waiss@gmail.com>
2018-07-11 19:59:49 +0200

    add vuejs

---
+++ handless/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      test: "baka"
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  props: [],
+  methods: {},
+  mounted() {}
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
